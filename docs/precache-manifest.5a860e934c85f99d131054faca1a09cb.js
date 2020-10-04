84a17ced943e81c0e33ffcb742aa02a9e0680637
Gabriel <leviatan21@gmail.com>
2020-10-04 12:31:50 -0300

    Docs agregado

---
+++ docs/precache-manifest.5a860e934c85f99d131054faca1a09cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86995a624121607f5c502a5d33c756d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "554c25826e64f486e626",
+    "url": "/static/css/main.b53b863d.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "554c25826e64f486e626",
+    "url": "/static/js/main.228eede6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
